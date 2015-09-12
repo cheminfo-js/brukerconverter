d1a8d765e143b30cecd2fbfd828fc265fe48de0f
jeffersonh44 <swrt_9950@hotmail.com>
2015-09-12 16:35:21 -0500

    FID and convert2D implemented without test

--- src/brukerconverter2.js
+++ src/brukerconverter2.js
@@ -24,13 +24,55 @@ function convert1D(files) {
     } else if(files['fid']) {
         result = parseData(files['pdata/1/procs']);
         parseData(files['acqus'], result);
-        setXYSpectrumData(files['fid'], result, 'fid', true)
+        setFIDSpectrumData(files['fid'], result, true)
     }
     return result;
 }
 
 function convert2D(files) {
+    var result = {};
+    var temp = {};
+    if(files['ser']) {
+        parseData(files['ser'], result);
+        parseData(files['acqu2s'], temp);
+    } else if(files['2rr']) {
+        parseData(files['2rr'], result);
+        parseData(files['proc2s'], temp);
+    }
 
+    var nbSubSpectra = temp['$SI'] = parseInt(temp['$SI']);
+    var SW_p = temp['$SWp'] = parseFloat(temp['$SWp']);
+    var SF = temp['$SF'] = parseFloat(temp['$SF']);
+    var offset = temp['$OFFSET'] = parseFloat(temp['$OFFSET']);
+
+    result.firstY = offset;
+    result.lastY = offset - SW_p/SF;
+    result['$BF2'] = SF;
+    result['$SF01'] = SF;
+
+    if(files['ser']) {
+        setXYSpectrumData(files['ser'], result, 'ser', true);
+        result.setYUnits = 'HZ';
+        result.dataType = 'TYPE_2DNMR_FID';
+    } else if(files['2rr']) {
+        setXYSpectrumData(files['2rr'], result, '2rr', true);
+        result.setYUnits = 'PPM';
+        result.dataType = 'TYPE_2DNMR_SPECTRUM';
+    }
+
+    for(var i = 0; i < nbSubSpectra; ++i) {
+        // TODO: go through all 2xx spectra?
+    }
+
+    // TODO: set active spectra?
+    result['2D_Y_NUCLEUS'] = temp['$AXNUC'];
+    result['2D_X_NUCLEUS'] = result['$AXNUC'];
+    result['2D_Y_FRECUENCY'] = SF;
+    result['2D_Y_OFFSET'] = offset;
+    result['2D_X_FRECUENCY'] = result['$SF'];
+    result['2D_X_OFFSET'] = result['$OFFSET'];
+
+    return result;
 }
 
 function setXYSpectrumData(file, spectra, store, real) {
@@ -61,11 +103,11 @@ function setXYSpectrumData(file, spectra, store, real) {
     var read;
     if(endian) {
         read = function (index) {
-            return file.readInt32LE(index * 4, 4)
+            return file.readInt32LE(index * 4, 4);
         };
     } else {
         read = function (index) {
-            return file.readInt32BE(index * 4, 4)
+            return file.readInt32BE(index * 4, 4);
         };
     }
 
@@ -130,5 +172,30 @@ function parseData(file, currentResult, options) {
     }
 
     return result;
+}
+
+function setFIDSpectrumData(file, spectra, real) {
+    var td = spectra['$TD'] = parseInt(spectra['$TD']);
+    var SW_p = spectra['$SWP'] = parseFloat(spectra['$SWP']);
+    if(SW_p !== 0) {
+        spectra['$SWH'] = SW_p;
+    }
+    var SW_h = spectra['$SWH'] = parseFloat(spectra['$SWH']);
+
+    var SF = spectra['$SF01'] = parseFloat(spectra['$SF01']);
+    var BF = spectra['$BF1'] = parseFloat(spectra['$BF1']);
+
+    spectra['$SF'] = parseFloat(spectra['$SF']);
+    if(spectra['$SF'] !== 0) {
+        BF = spectra['$SF'];
+    }
+
+    var DW = 1 / (2 * SW_h);
+    var AQ = td * DW;
+
+    var endian = parseInt(spectra["$BYTORDP"]);
+    endian = endian ? 0 : 1;
+
+    // TODO: which two spectras I have to read?
 }
 
